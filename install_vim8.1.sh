41a50ded357a95f04ecf7c74df329d77d82c305a
maning <maning1x@163.com>
2019-07-20 21:57:32 +0800

    make install shell script
    Signed-off-by: maning <maning1x@163.com>

---
+++ install_vim8.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# install_vim8.1.sh
+#
+# Copyright (C) 2018-2023 Eric MA  <eric@company.com>
+#
+# History:
+#    2019/07/20 - [Eric MA] Created file
+#
+
+
+tar -zxvf vim_8.1.573.tar.gz
+cd vim
+sudo make install
